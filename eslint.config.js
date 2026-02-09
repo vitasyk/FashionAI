8bda9d66b96500be2d2e1f14f7daef957b75f0a4
a48-vitasyk <vitaly.stetsyuk@zomro.org>
2026-02-09 23:29:55 +0200

    security: exhaustive .gitignore and fix eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,20 +1,28 @@
-{
-    "$schema": "https://ui.shadcn.com/schema.json",
-        "style": "default",
-            "rsc": false,
-                "tsx": true,
-                    "tailwind": {
-        "config": "tailwind.config.ts",
-            "css": "src/index.css",
-                "baseColor": "slate",
-                    "cssVariables": true,
-                        "prefix": ""
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+    { ignores: ["dist"] },
+    {
+        extends: [js.configs.recommended, ...tseslint.configs.recommended],
+        files: ["**/*.{ts,tsx}"],
+        languageOptions: {
+            ecmaVersion: 2020,
+            globals: globals.browser,
+        },
+        plugins: {
+            "react-hooks": reactHooks,
+            "react-refresh": reactRefresh,
+        },
+        rules: {
+            ...reactHooks.configs.recommended.rules,
+            "react-refresh/only-export-components": [
+                "warn",
+                { allowConstantExport: true },
+            ],
+        },
     },
-    "aliases": {
-        "components": "@/components",
-            "utils": "@/lib/utils",
-                "ui": "@/components/ui",
-                    "lib": "@/lib",
-                        "hooks": "@/hooks"
-    }
-}
+);
